ae512563d5d4f43e0d72010384a6777a8c45bbb9
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-05-10 23:19:59 +0300

    front - connection button added

--- front/src/main/scala/org/djnzx/Main.scala
+++ front/src/main/scala/org/djnzx/Main.scala
@@ -4,38 +4,51 @@ import cats.effect.IO
 import scala.scalajs.js.annotation.*
 import tyrian.*
 import tyrian.Html.*
+import tyrian.websocket.*
 
 @JSExportTopLevel("TyrianApp")
 object Main extends TyrianIOApp[Msg, Model]:
 
-  def router: Location => Msg =
-    Routing.none(Msg.NoOp)
+  private val wsUrl = "ws://localhost:8081/ws"
+
+  def router: Location => Msg = Routing.none(Msg.NoOp)
 
   def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
-    (Nil, Cmd.None)
+    (Model(Nil, None), Cmd.None)
 
   def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
-    case Msg.Insert =>
-      (Counter.init :: model, Cmd.None)
 
-    case Msg.Remove =>
-      model match
-        case Nil    => (Nil, Cmd.None)
-        case _ :: t => (t, Cmd.None)
-
-    case Msg.Modify(id, m) =>
-      val updated = model.zipWithIndex.map { case (c, i) =>
-        if i == id then Counter.update(m, c) else c
+    case Msg.Connect =>
+      val cmd = WebSocket.connect[IO, Msg](wsUrl) {
+        case WebSocketConnect.Socket(ws) => Msg.WsOpen(ws)
+        case WebSocketConnect.Error(_)   => Msg.NoOp
       }
+      (model, cmd)
 
-      (updated, Cmd.None)
+    case Msg.WsOpen(ws) =>
+      (model.copy(ws = Some(ws)), Cmd.None)
 
-    case Msg.NoOp =>
+    case Msg.WsReceive(s) =>
       (model, Cmd.None)
 
-  /** page view */
+    case Msg.Insert =>
+      (model.copy(counters = Counter.init :: model.counters), Cmd.None)
+
+    case Msg.Remove =>
+      model.counters match
+        case Nil    => (model, Cmd.None)
+        case _ :: t => (model.copy(counters = t), Cmd.None)
+
+    case Msg.Modify(id, m) =>
+      val updated = model.counters.zipWithIndex.map { case (c, i) =>
+        if i == id then Counter.update(m, c) else c
+      }
+      (model.copy(counters = updated), Cmd.None)
+
+    case Msg.NoOp => (model, Cmd.None)
+
   def view(model: Model): Html[Msg] =
-    val counters = model.zipWithIndex.map { case (c, i) =>
+    val counters = model.counters.zipWithIndex.map { case (c, i) =>
       Counter.view(c).map(msg => Msg.Modify(i, msg))
     }
 
@@ -43,34 +56,33 @@ object Main extends TyrianIOApp[Msg, Model]:
       Header.view ::
         List(
           div(cls := "flex space-x-4")(
-            button(
-              onClick(Msg.Remove),
-              cls := "px-2 bg-sky-200 rounded"
-            )(
-              text("remove")
-            ),
-            button(
-              onClick(Msg.Insert),
-              cls := "px-2 bg-sky-200 rounded"
-            )(
-              text("insert")
-            )
+            button(onClick(Msg.Remove), cls := "px-2 bg-sky-200 rounded")(text("remove")),
+            button(onClick(Msg.Insert), cls := "px-2 bg-sky-200 rounded")(text("insert")),
+            button(onClick(Msg.Connect), cls := "px-2 bg-green-200 rounded")(text("connect ws"))
           )
         ) ++ counters
 
-    div(cls := "p-4")(
-      div(cls := "space-y-4")(elems*)
-    )
+    div(cls := "p-4")(div(cls := "space-y-4")(elems*))
 
   def subscriptions(model: Model): Sub[IO, Msg] =
-    Sub.None
+    model.ws.fold(Sub.None) { ws =>
+      ws.subscribe {
+        case WebSocketEvent.Open        => Msg.NoOp
+        case WebSocketEvent.Receive(s)  => Msg.WsReceive(s)
+        case WebSocketEvent.Error(_)    => Msg.NoOp
+        case WebSocketEvent.Close(_, _) => Msg.NoOp
+        case WebSocketEvent.Heartbeat   => Msg.NoOp
+      }
+    }
 
-type Model = List[Counter.Model]
+case class Model(counters: List[Counter.Model], ws: Option[WebSocket[IO]])
 
 enum Msg:
-  case Insert
-  case Remove
+  case Insert, Remove
   case Modify(i: Int, msg: Counter.Msg)
+  case Connect
+  case WsOpen(ws: WebSocket[IO])
+  case WsReceive(s: String)
   case NoOp
 
 object Counter:
@@ -82,22 +94,11 @@ object Counter:
   enum Msg:
     case Increment, Decrement
 
-  /** counter view */
   def view(model: Model): Html[Msg] =
     div(cls := "flex space-x-4")(
-      button(
-        onClick(Msg.Decrement),
-        cls := "px-2 bg-red-300 rounded"
-      )(
-        text("-")
-      ),
+      button(onClick(Msg.Decrement), cls := "px-2 bg-red-300 rounded")("-"),
       p(cls := "text-lg font-medium")(model.toString),
-      button(
-        onClick(Msg.Increment),
-        cls := "px-2 bg-lime-300 rounded"
-      )(
-        text("+")
-      )
+      button(onClick(Msg.Increment), cls := "px-2 bg-lime-300 rounded")("+")
     )
 
   def update(msg: Msg, model: Model): Model =
